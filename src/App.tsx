87d5a2e4de11f7decbda7c196a29b6adcf2f72a3
lonileere <32738539+lonileere@users.noreply.github.com>
2023-03-08 22:48:52 +0000

    💄 basic search page

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,23 @@
 import React from "react";
 import { Button } from "./ui/Button";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import "./App.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function App() {
   return (
-    <div>
-      <div className="w-full flex flex-col">
-        <div className="mx-auto flex flex-col w-full max-w-3xl">
-          <header>
-            <h1 className="text-6xl mb-4 md:text-7xl">What's the weather?</h1>
-          </header>
-          <h2 className="mb-2">
-            Search for your city in the input field below
-          </h2>
-          <input className="border border-gray-400 rounded-md outline-gray-500 py-2 px-4 mb-4"></input>
-          <Button
-            tailwind="w-48 self-center"
-            onClick={() => null}
-            primary={true}
-          >
-            <span className="text-xl">Search</span>
-          </Button>
-        </div>
-      </div>
-    </div>
+    <section className="mx-5 flex-1 md:mx-auto flex flex-col max-w-3xl self-center md:self-start">
+      <header>
+        <h1 className="text-6xl mb-4 md:text-7xl">How's the weather?</h1>
+      </header>
+      <h2 className="mb-2">Search for your city in the input field below</h2>
+      <input className=" w-full border border-gray-400 rounded-md outline-gray-500 py-2 px-4 mb-4"></input>
+      <Button tailwind="w-48 self-center" onClick={() => null} primary={true}>
+        <span className="text-xl font-bold">
+          <FontAwesomeIcon icon={faSearch}></FontAwesomeIcon> Search
+        </span>
+      </Button>
+    </section>
   );
 }
 
