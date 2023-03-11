7ed421b56bd35deaae07665de8e964101113e288
lonileere <32738539+lonileere@users.noreply.github.com>
2023-03-11 00:21:39 +0000

    💄 results page

---
+++ src/pages/Results/index.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { WeatherCard } from "../../ui/WeatherCard";
+
+interface Props {}
+
+export const Results: React.FC<Props> = () => {
+  return (
+    <>
+      <h1 className="text-6xl mb-4 md:text-7xl">London, UK</h1>
+      <WeatherCard temperature={25} />
+      <div className="flex flex-col space-y-2 md:flex-row md:space-x-2 md:space-y-0">
+        <WeatherCard small temperature={30} />
+        <WeatherCard small temperature={35} />
+        <WeatherCard small temperature={40} />
+        <WeatherCard small temperature={45} />
+      </div>
+    </>
+  );
+};
