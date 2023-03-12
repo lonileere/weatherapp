60148aee848b6421b381b2f3c1750b679cb8a662
lonileere <32738539+lonileere@users.noreply.github.com>
2023-03-12 23:05:39 +0000

    💄 spacing

--- src/pages/Results/index.tsx
+++ src/pages/Results/index.tsx
@@ -8,7 +8,7 @@ export const Results: React.FC<Props> = () => {
     <>
       <h1 className="text-6xl mb-4 md:text-7xl">London, UK</h1>
       <WeatherCard temperature={25} />
-      <div className="flex flex-col space-y-2 md:flex-row md:space-x-2 md:space-y-0">
+      <div className="flex flex-col space-y-4 md:flex-row md:space-x-4 md:space-y-0">
         <WeatherCard small temperature={30} />
         <WeatherCard small temperature={35} />
         <WeatherCard small temperature={40} />
