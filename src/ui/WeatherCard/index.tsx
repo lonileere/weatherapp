88f49adf6b60df480effbaa1dce6d0ec613e8617
lonileere <32738539+lonileere@users.noreply.github.com>
2023-03-17 18:05:57 +0100

    ✨ component improvements

--- src/ui/WeatherCard/index.tsx
+++ src/ui/WeatherCard/index.tsx
@@ -1,20 +1,60 @@
 import React from "react";
-import { faCloud } from "@fortawesome/free-solid-svg-icons";
+import {
+  faCloud,
+  faSun,
+  faCloudRain,
+  faThunderstorm,
+  faSnowflake,
+  faSmog,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 interface Props {
-  weather: string | undefined;
-  temp: number | undefined;
+  weather: string;
+  temp: number;
+  date: string;
 }
 
-export const WeatherCard: React.FC<Props> = ({ weather, temp }) => {
+const weatherIcon = (weather: string) => {
+  switch (weather) {
+    case "Rain": {
+      return <FontAwesomeIcon icon={faCloudRain} />;
+    }
+    case "Clouds": {
+      return <FontAwesomeIcon icon={faCloud} />;
+    }
+    case "Clear": {
+      return <FontAwesomeIcon icon={faSun} />;
+    }
+    case "Thunderstorm": {
+      return <FontAwesomeIcon icon={faThunderstorm} />;
+    }
+    case "Drizzle": {
+      return <FontAwesomeIcon icon={faCloudRain} />;
+    }
+    case "Snow": {
+      return <FontAwesomeIcon icon={faSnowflake} />;
+    }
+    default: {
+      return <FontAwesomeIcon icon={faSmog} />;
+    }
+  }
+};
+
+export const WeatherCard: React.FC<Props> = ({ weather, temp, date }) => {
   return (
-    <section className="flex">
-      <span className={"text-8xl"}>
-        {weather}
-        <FontAwesomeIcon icon={faCloud} />
+    <section className="grid grid-rows-2 grid-cols-1 md:grid-cols-2 mb-5">
+      <span
+        className={
+          "flex justify-center text-9xl md:col-end-2 md:row-span-2 text-center"
+        }
+      >
+        {weatherIcon(weather)}
       </span>
-      <span className={"text-5xl ml-5"}>{temp}°C</span>
+      <span className={"text-5xl md:text-7xl ml-5 md:col-start-2"}>
+        {temp}°C
+      </span>
+      <span className={"text-3xl md:text-5xl ml-5 md:col-start-2"}>{date}</span>
     </section>
   );
 };
